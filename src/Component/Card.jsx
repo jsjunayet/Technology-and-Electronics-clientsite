79d98d1a9bb386a81d4401eb3f572608ebaea114
Junayet Shiblu <130874714+jsjunayet@users.noreply.github.com>
2023-10-21 23:25:57 +0600

    added Technology and Electronics website complete

--- src/Component/Card.jsx
+++ src/Component/Card.jsx
@@ -12,7 +12,7 @@ const Card = () => {
 
         Swal.fire({
             title: 'Are you sure?',
-            text: "You won't be able to revert this!",
+            text: "You are won't be able to revert this!",
             icon: 'warning',
             showCancelButton: true,
             confirmButtonColor: '#3085d6',
