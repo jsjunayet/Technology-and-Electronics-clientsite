84fefc9c33186d3418b3d260c0fe15298716fb44
Junayet Shiblu <130874714+jsjunayet@users.noreply.github.com>
2023-10-21 23:07:47 +0600

    added my client site all requirement complete

--- src/Component/Main/Home.jsx
+++ src/Component/Main/Home.jsx
@@ -51,7 +51,7 @@ const Home = () => {
                         }}
                         data-swiper-parallax="-23%"
                     ></div>
-                    <SwiperSlide className='lg:mt-10 mt-2'>
+                    <SwiperSlide className='lg:mt-20 mt-1'>
                         <div className="lg:text-5xl lg:text-bold text-2xl font-semibold text-center" data-swiper-parallax="-300">
                             Technology and Electronics
                         </div>
@@ -59,12 +59,12 @@ const Home = () => {
                             Apply
                         </div>
                         <div className="text" data-swiper-parallax="-100">
-                            <p className='px-2 lg:px-20 lg:text-xl text-sm'>
+                            <p className='px-1 lg:px-20 lg:text-xl text-sm'>
                                 Apple Inc., founded in 1976 by Steve Jobs, Steve Wozniak, and Ronald Wayne, is a prominent American multinational technology company. Renowned for its cutting-edge consumer electronics and software, Apple has an illustrious product line that includes the iPhone, iPad, Mac, iPod, Apple Watch, and Apple TV. Notable for its sleek design and user-friendly interface, Apple's products have garnered a devoted global following. Its software offerings, such as macOS, iOS, iTunes, Safari, and various productivity suites, are widely acclaimed for their functionality and integration. The company's online services, including the App Store, iTunes Store, Apple Music, and iCloud, further contribute to its comprehensive technological ecosystem. With an unwavering commitment to innovation and quality, Apple has solidified its position as one of the world's leading and most recognizable brands in the technology sector
                             </p>
                         </div>
                     </SwiperSlide>
-                    <SwiperSlide className='lg:mt-10 mt-2'>
+                    <SwiperSlide className='lg:mt-16 mt-1'>
                         <div className="lg:text-5xl lg:text-bold text-2xl font-semibold text-center" data-swiper-parallax="-300">
                             Technology and Electronics
                         </div>
@@ -72,20 +72,20 @@ const Home = () => {
                             Samsung
                         </div>
                         <div className="text" data-swiper-parallax="-100">
-                            <p className='px-2 lg:px-20 lg:text-xl text-sm'>
+                            <p className='px-1 lg:px-20 lg:text-xl text-sm'>
                                 Samsung, a South Korean multinational conglomerate, is a key player in the global technology industry. Established in 1938, the company has diversified its operations across various sectors, including consumer electronics, mobile devices, semiconductors, and home appliances. Renowned for its innovative and high-quality products, Samsung's extensive range encompasses smartphones, such as the Galaxy series, along with tablets, laptops, and smartwatches. The company's semiconductor division is a leading producer of memory chips and other semiconductor components, playing a crucial role in the global technology supply chain. Samsung's home appliance segment is also widely recognized for its modern design and advanced functionalities. With a strong emphasis on research and development, Samsung continues to be a prominent name in the global tech market, consistently pushing the boundaries of technological advancement.
                             </p>
                         </div>
                     </SwiperSlide>
-                    <SwiperSlide className='lg:text-5xl lg:text-bold text-2xl font-semibold text-center'>
-                        <div className="lg:title text-2xl" data-swiper-parallax="-300">
+                    <SwiperSlide className='lg:text-5xl lg:text-bold text-2xl lg:mt-10 font-semibold text-center'>
+                        <div className="lg:text-5xl text-2xl" data-swiper-parallax="-300">
                             Technology and Electronics
                         </div>
                         <div className="subtitle text-4xl font-semibold" data-swiper-parallax="-200">
                             Google
                         </div>
                         <div className="text" data-swiper-parallax="-100">
-                            <p className='px-2 lg:px-20 lg:text-xl text-sm'>
+                            <p className='px-1 lg:px-20 lg:text-xl text-sm'>
                                 Google, an American multinational technology company founded in 1998 by Larry Page and Sergey Brin, is a dominant force in the digital landscape. Known primarily for its internet-related services and products, Google has an expansive portfolio that includes the world's most widely used search engine, along with other offerings such as Google Maps, Google Chrome, and Google Workspace. The company's Android operating system is the most popular mobile operating system globally, powering a wide range of smartphones and other devices. Google's innovative approach to technology has led to the development of various hardware products, including the Pixel smartphone, Chromecast, and Google Home smart speakers. Additionally, the company's forays into artificial intelligence and machine learning have significantly impacted the advancement of these fields on a global scale. With its ubiquitous presence and continuous focus on technological innovation, Google remains a central pillar in the modern digital era.
                             </p>
                         </div>
--- src/Component/Main/Navigate/Section.jsx
+++ src/Component/Main/Navigate/Section.jsx
@@ -12,7 +12,7 @@ const Section = () => {
                             <p className="py-6">The rapid advancements in technology and electronics have not only streamlined everyday tasks but have also facilitated the development of innovative solutions for complex global challenges. From the proliferation of smart home devices to the integration of artificial intelligence in various sectors, the transformative impact of technology continues to redefine the way people live and work, fostering unprecedented levels of interconnectedness and efficiency. As the world embraces a digital future, the integration of technology and electronics is expected to play an increasingly central role in shaping the way societies function and progress.</p>
                             <div className="flex items-center gap-5">
                                 <input type="text" placeholder="Type here" className="input input-bordered input-primary w-full max-w-xs" />
-                                <button className="bg-[#97C33C] px-3 py-[10px] text-black text-xl rounded hover:bg-slate-200">Get Started</button>
+                                <button className="bg-[#97C33C] lg:px-3 px-1 lg:py-[10px] py-[5px] text-black lg:text-xl text-sm rounded hover:bg-slate-200">Get Started</button>
 
                             </div>
                         </div>
